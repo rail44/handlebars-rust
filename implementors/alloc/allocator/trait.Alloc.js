c6b8facbec3230ba482c3cfd187508fa16170f28
Ning Sun <sunng@about.me>
2017-08-01 22:38:02 +0800

    (cargo-release) generate docs

--- implementors/alloc/allocator/trait.Alloc.js
+++ implementors/alloc/allocator/trait.Alloc.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["handlebars"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/allocator/trait.Alloc.html\" title=\"trait alloc::allocator::Alloc\">Alloc</a> for &amp;'a System","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/allocator/trait.Alloc.html\" title=\"trait alloc::allocator::Alloc\">Alloc</a> for System",];
+implementors["handlebars"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/allocator/trait.Alloc.html\" title=\"trait alloc::allocator::Alloc\">Alloc</a> for System","impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/allocator/trait.Alloc.html\" title=\"trait alloc::allocator::Alloc\">Alloc</a> for &amp;'a System",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
